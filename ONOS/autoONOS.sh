fe9d4738163d39b4d5337281dc7dde797913c270
Jianwei Mao <maojianwei2020@gmail.com>
2018-04-03 17:48:08 +0800

    update usage

--- ONOS/autoONOS.sh
+++ ONOS/autoONOS.sh
@@ -9,7 +9,7 @@ cd /home/mao/onos
 # First, modify the first line of me. Recommend to put ONOS source code in your home directory
 # 
 # Update, build, and run ONOS:             ./autoONOS.sh
-# Stop ONOS and Clean all executive file:  ./autoONOS.sh -c
+# Stop ONOS and Clean all executive files:  ./autoONOS.sh -c
 # -----------------------------
 
 
