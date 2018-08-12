df46c47f85d7c10aac2e92880b73d871b868f031
Jianwei Mao <maojianwei2020@gmail.com>
2018-08-12 17:59:35 +0800

    auto push live stream to live platform, e.g. Douyu, Youtube

---
+++ others/pushLiveStream.sh
@@ -0,0 +1,20 @@
+
+# Infinitely loop:   -stream_loop -1
+# Not use -re, because of easily blurred screen
+
+video_src=""
+video_src_port=""
+
+live_addr="" # e.g.  rtmp://send1a.douyu.com/live , copy from your live platform
+live_push_code="" # copy from your live platform too
+
+count=0
+while true
+do
+    ffmpeg -stream_loop -1 -i "rtsp://${video_src}:${video_src_port}/" -acodec copy -vcodec copy -f flv -y "${live_addr}/${live_push_code}"
+    ((count++))
+    echo ""
+    echo "--- Mao retry ... $count ---"
+    echo ""
+    sleep 1
+done
