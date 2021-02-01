86734215bb87e082a4663f381c972b8ca230237f
Jianwei Mao <maojianwei2012@126.com>
2021-02-01 10:40:42 +0800

    Create mao_compile_kernel.sh

---
+++ Linux_Kernel/mao_compile_kernel.sh
@@ -0,0 +1,37 @@
+LOG=/home/mao/mao_kernel_build.html
+
+if [ "$(whoami)" != "root" ]
+then
+    echo "Mao: we need root privilege to compile & install Linux Kernel."
+    echo "Mao: we need root privilege to compile & install Linux Kernel." > ${LOG}
+    exit 1
+fi
+
+
+BUILD_TIME=`date`
+echo "============================== Mao Kernel Build - ${BUILD_TIME} =============================="
+echo "============================== Mao Kernel Build - ${BUILD_TIME} ==============================" > ${LOG}
+
+echo "============================== Mao Kernel Build - menuconfig  =============================="
+echo "============================== Mao Kernel Build - menuconfig  ==============================" >> ${LOG}
+make menuconfig
+
+echo "============================== Mao Kernel Build - make -j48  =============================="
+echo "============================== Mao Kernel Build - make -j48  ==============================" >> ${LOG}
+make -j48 >> ${LOG}
+
+echo "============================== Mao Kernel Build - make modules_install  =============================="
+echo "============================== Mao Kernel Build - make modules_install  ==============================" >> ${LOG}
+make modules_install >> ${LOG}
+
+echo "============================== Mao Kernel Build - make headers_install INSTALL_HDR_PATH=/usr  =============================="
+echo "============================== Mao Kernel Build - make headers_install INSTALL_HDR_PATH=/usr  ==============================" >> ${LOG}
+make headers_install INSTALL_HDR_PATH=/usr >> ${LOG}
+
+echo "============================== Mao Kernel Build - menuconfig  =============================="
+echo "============================== Mao Kernel Build - menuconfig  ==============================" >> ${LOG}
+make install >> ${LOG}
+
+FINISH_TIME=`date`
+echo "============================== Mao Kernel Build - Done - $BUILD_TIME -> ${FINISH_TIME} =============================="
+echo "============================== Mao Kernel Build - Done - $BUILD_TIME -> ${FINISH_TIME} ==============================" >> ${LOG}
